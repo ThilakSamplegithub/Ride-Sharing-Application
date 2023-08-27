dea41ccf129a6e67f416b5fce8d3fc312aebcd46
nikhil-pillare <nikhilpillare98@gmail.com>
2023-08-27 23:35:34 +0530

    nik-final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Passenger from './Pages/passenger';
+import Rider from './Pages/rider';
+import Navbar from './Components/NavBar';
+import MainRoutes from './Pages/MainRoutes';
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+    <MainRoutes/>
+    
+    
     </div>
   );
 }
