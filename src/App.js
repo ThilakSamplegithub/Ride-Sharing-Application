064bdcc526689cfcad6b67046e682e8df44274d8
ThilakSamplegithub <112916543+ThilakSamplegithub@users.noreply.github.com>
2024-02-11 13:53:02 +0530

    did removed navbar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import Passenger from './Pages/passenger';
 import Rider from './Pages/rider';
-import Navbar from './Components/NavBar';
 import MainRoutes from './Pages/MainRoutes';
 import WithSubnavigation from './Components/Navbar2';
 function App() {
